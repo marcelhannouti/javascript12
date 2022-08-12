52a8a42ed842cdeec11ca86737a898b21fe0cf5f
marc <marc.hannouti360@gmail.com>
2022-08-12 10:55:26 +0200

    commit

---
+++ 1.js
@@ -0,0 +1,10 @@
+function numero (...param){
+    param.forEach((param) =>{
+        console.log(param)
+    })
+    }
+    numero("a","b","c","d","e")
+
+for (let index = 0; index < 6; index++) {
+console.log(index)    
+}
